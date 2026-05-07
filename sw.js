89b27b95bfe94f077740df819a41fde89ef3c986
alandpr <81269864+alandpr@users.noreply.github.com>
2026-05-07 19:53:46 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,34 +1,20 @@
-const CACHE_NAME = 'cm-v10.5';
-const ASSETS = [
+const CACHE_NAME = 'cm-v10-8';
+const assets = [
   './',
-  './index.html',
-  './manifest.json',
-  'https://cdn-icons-png.flaticon.com/192/3233/3233486.png',
-  'https://cdn-icons-png.flaticon.com/512/3233/3233486.png'
+  'index.html',
+  'manifest.json'
 ];
 
-// Instalação e Cache
-self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(assets))
   );
-  self.skipWaiting();
 });
 
-// Limpeza de caches antigos
-self.addEventListener('activate', (e) => {
-  e.waitUntil(
-    caches.keys().then((keys) => {
-      return Promise.all(
-        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
-      );
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(cachedResponse => {
+      return cachedResponse || fetch(event.request);
     })
-  );
-});
-
-// Resposta Offline
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((res) => res || fetch(e.request))
   );
 });
