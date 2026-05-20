b22c8eede23560b2e2801b66f9266ca9f4adb2e6
alandpr <alanpocos@gmail.com>
2026-05-20 16:53:03 -0300

    update

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'cm-v28-final-manual';
+const CACHE_NAME = 'cm-v29-final-manual';
 const assets = ['./', 'index.html', 'manifest.json'];
 
 self.addEventListener('install', event => {
